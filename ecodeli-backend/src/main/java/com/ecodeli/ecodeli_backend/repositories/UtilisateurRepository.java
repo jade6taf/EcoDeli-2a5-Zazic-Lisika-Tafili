b052713bafc807e0f5da2323aff86197c009ebce
Ivan Zazic <zazicivan@gmail.com>
2025-04-06 18:13:41 +0200

    [ADD] repositories for 'utilisateur' only

--- ecodeli-backend/src/main/java/com/ecodeli/ecodeli_backend/repositories/UtilisateurRepository.java
+++ ecodeli-backend/src/main/java/com/ecodeli/ecodeli_backend/repositories/UtilisateurRepository.java
@@ -4,9 +4,16 @@ import com.ecodeli.ecodeli_backend.models.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
     Optional<Utilisateur> findByEmail(String email);
+
+    List<Utilisateur> findByType(String type);
+
+    List<Utilisateur> findByEntrepriseIdEntreprise(Integer idEntreprise);
+
+    boolean existsByEmail(String email);
 }
