4f70a13c4eed1b750a71d21ee087026cf3c76dc1
Ivan Zazic <zazicivan@gmail.com>
2025-05-27 13:02:14 +0200

    [FEAT] add macro to .properties with .env to get more secure

--- ecodeli-backend/src/main/java/com/ecodeli/ecodeli_backend/EcodeliBackendApplication.java
+++ ecodeli-backend/src/main/java/com/ecodeli/ecodeli_backend/EcodeliBackendApplication.java
@@ -1,5 +1,6 @@
 package com.ecodeli.ecodeli_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,7 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EcodeliBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.ignoreIfMissing()
+				.load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(EcodeliBackendApplication.class, args);
 	}
-
 }
